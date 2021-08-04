ec134706f87030cfcd8db9f4b9d5c10ce725adb9
rodrigohartl <82826011+rodrigohartl@users.noreply.github.com>
2021-08-04 18:53:24 -0300

    Add files via upload

---
+++ Games/Hangman Game/hangman.py
@@ -0,0 +1,25 @@
+def hangman ():
+    stage = 0
+    wrong_guesses = [ "" , "________ " , "| | " , "| 0 " , "|/|\ " , "|/ \ " , "| " ]
+    word = "cat"
+    score_board = [ "__" ] * len (word)
+    win = False
+    print ( "Welcome to Hang Man" )
+    while stage < len (wrong_guesses) - 1 :
+        print ( " \n " )
+        guess = input ( "Guess a letter" )
+        if guess in word:
+            score_board[word.index(guess)] = guess
+        else :
+            stage += 1
+        print (( " " .join(score_board)))
+        print ( " \n " .join(wrong_guesses[ 0 : stage + 1 ]))
+        if "__" not in score_board:
+            print ( "You win! The word was:" )
+            print ( " " .join(score_board))
+            win = True
+            break
+    if not win:
+        print ( " \n " .join(wrong_guesses[ 0 : stage]))
+        print ( "You lose!" )
+hangman()
